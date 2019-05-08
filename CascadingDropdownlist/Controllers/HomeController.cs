f0d668dc11f643b2d8cb009fe01107f3deee12f6
Admin <eaak2004@hotmail.com>
2019-05-08 11:16:12 +0100

    Add project files.

---
+++ CascadingDropdownlist/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using CascadingDropdownlist.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CascadingDropdownlist.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            List<Student> students = new List<Student>();
+
+            using (StudentDBContext db = new StudentDBContext())
+            {
+                students = db.Students.ToList();
+            }
+
+            return View(students);
+        }
+
+        [HttpGet]
+        public ActionResult NewUpdateStudent()
+        {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public ActionResult NewUpdateStudent(Student student, int? contry, int? state)
+        {
+            if (contry != null)
+            {
+                ViewData["CountryId"] = contry;
+            }
+
+            if (state != null)
+            {
+                ViewData["StateId"] = state;
+            }
+
+
+            if (ModelState.IsValid)
+            {
+                using (StudentDBContext db = new StudentDBContext())
+                {
+                    db.Students.Add(student);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index", "Home");
+                }
+            }
+
+            return View();
+        }
+    }
+}
---
+++ CascadingDropdownlist/ViewModels/Students/StudentViewModel.cs
@@ -0,0 +1,62 @@
+﻿using CascadingDropdownlist.Models.DB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CascadingDropdownlist.ViewModels.Students
+{
+    public class StudentViewModel
+    {
+        public static SelectList GetAllCountries()
+        {
+            List<SelectListItem> countries = new List<SelectListItem>();
+
+            using (StudentDBContext db = new StudentDBContext())
+            {
+                countries = db.Countries.Select(x => new SelectListItem
+                            {
+                                Value = x.CountryId.ToString(),
+                                Text = x.Country1
+                            }).ToList();
+            }
+
+            SelectList selectList = new SelectList(countries.AsEnumerable(), "Value", "Text");
+
+            return selectList;
+        }
+
+        public static SelectList GetStatesAccordingToCountries(int countryId)
+        {
+            List<SelectListItem> states = new List<SelectListItem>();
+
+            using (StudentDBContext db = new StudentDBContext())
+            {
+                states = db.States
+                        .Where(x => x.CountryId == countryId)
+                        .Select(x => new SelectListItem { Value = x.StateId.ToString(), Text = x.StateName }).ToList();
+            }
+
+            SelectList selectList = new SelectList(states.AsEnumerable(), "Value", "Text");
+
+            return selectList;
+        }
+
+        public static SelectList GetCitiesAccordingToStates(int stateId)
+        {
+            List<SelectListItem> cities = new List<SelectListItem>();
+
+            using (StudentDBContext db = new StudentDBContext())
+            {
+                cities = db.Cities
+                        .Where(x => x.StatedId == stateId)
+                        .Select(x => new SelectListItem { Value = x.CityId.ToString(), Text = x.CityName }).ToList();
+            }
+
+            SelectList selectList = new SelectList(cities.AsEnumerable(), "Value", "Text");
+
+            return selectList;
+        }
+    }
+}
